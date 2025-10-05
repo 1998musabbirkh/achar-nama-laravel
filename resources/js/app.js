8d12808794a95333fce8e5dee3921c10b6845498
1998musabbirkh <1998musabbirkh@gmail.com>
2025-10-06 02:39:51 +0600

    added css file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import './bootstrap';
+import '../css/app.css'
 import '../images/banner.webp'
 import '../images/google.png'
 import '../images/logo.png'
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard | @yield('title')</title>
     @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
 
 <body class="bg-bg">
